27c5733163ae3027950235dcc6da0779bd769f12
Rogalik <rogalski92@hotmail.com>
2018-04-16 13:49:11 +0200

    Commit

---
+++ src/com/javarush/test/level04/lesson02/task03/Cat.java
@@ -0,0 +1,18 @@
+package com.javarush.test.level04.lesson02.task03;
+
+/* Implement method setCatsCount()
+Implement the setCatsCount method, so the value of the catsCount variable would be equal to the passed parameter.
+*/
+
+public class Cat {
+    private static int catsCount = 0;
+
+    public static void main(String [] args) {
+        setCatsCount(1);
+    }
+
+    public static void setCatsCount(int catsCount) {
+        Cat.catsCount = catsCount;
+        catsCount++;
+    }
+}
