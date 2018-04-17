b0b342c8d4b0743ac9dd41f24a7742b9710349aa
Rogalik <rogalski92@hotmail.com>
2018-04-17 21:06:23 +0200

    Commit

--- src/com/javarush/test/level05/lesson12/bonus02/Solution.java
+++ src/com/javarush/test/level05/lesson12/bonus02/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.test.level05.lesson12.bonus02;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /* Add new functionality to the program
 Old Task: add a new function that reads from keyboard two numbers and displays their minimum.
@@ -16,15 +17,32 @@ public class Solution
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(reader.readLine());
         int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+        int e = Integer.parseInt(reader.readLine());
 
-        int minimum = min(a, b);
-
-        System.out.println("Minimum = " + minimum);
+        int minimumFive = minFive(a,b,c,d,e);
+        System.out.println("MinimumFive = " + minimumFive);
     }
-
-
     public static int min(int a, int b)
     {
         return a < b ? a : b;
     }
+    public static int minFive(int a, int b, int c,int d,int e) {
+        if(min(a,b) < min(c,d) ) {
+            return(min(min(a,b),e));
+        } else {
+            return(min(min(c,d),e));
+        }
+    }
 }
+
+//    int[] myArray = new int[5];
+//        myArray[0] = a;
+//                myArray[1] = b;
+//                myArray[2] = c;
+//                myArray[3] = d;
+//                myArray[4] = e;
+//
+//                Arrays.sort(myArray);
+//                System.out.println(myArray[0]);
---
+++ src/com/javarush/test/level05/lesson12/bonus03/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.test.level05.lesson12.bonus03;
+
+import java.io.*;
+
+/* Learning and practicing algorithm
+Task: Write a program that
+    1. reads from the console number N > 0
+    2. then reads from the console N numbers
+    3. displays the maximum of N numbers typed in.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int maximum = ;
+
+        //add your code here
+
+
+
+
+        System.out.println(maximum);
+    }
+}
