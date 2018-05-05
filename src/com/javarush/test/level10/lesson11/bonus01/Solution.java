7958d9cf0ac6caf38667a4fde577dc6671e05095
Rogalik <rogalski92@hotmail.com>
2018-05-05 16:10:32 +0200

    Ostatnie zadania

---
+++ src/com/javarush/test/level10/lesson11/bonus01/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.test.level10.lesson11.bonus01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/* Program doesn’t compile and run. Fix it.
+Task: The program shows how HashMap works. The program reads from the keyboard a set of pairs (a number and a string), places them in a HashMap and displays to the screen the contents of HashMap.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws IOException
+    {
+        Scanner scanner = new Scanner(System.in);
+        HashMap<Integer, String> map = new HashMap<Integer, String>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            int index = scanner.nextInt();
+            String name = scanner.nextLine();
+            map.put(index, name);
+        }
+
+        for (Map.Entry<Integer, String> pair : map.entrySet())
+        {
+            int id = pair.getKey();
+            String name = pair.getValue();
+            System.out.println("Id=" + id + " Name=" + name);
+        }
+    }
+}
---
+++ src/com/javarush/test/level10/lesson11/bonus02/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.test.level10.lesson11.bonus02;
+
+import java.io.*;
+import java.util.*;
+
+/* Add new functionality to the program.
+Old task: The program should display a pair (a number and a string) entered from the keyboard.
+    New task: The program should store in HashMap pairs (a number and a string) entered from the keyboard. An empty string means the end of input. Numbers may be repeated. Strings are always unique. Input data must not be lost!
+    The program should display the contents of HashMap to the screen.
+
+    Example input:
+    1
+    Stop
+    2
+    Look
+
+    Example output:
+    1 Stop
+    2 Look
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Map<String, Integer> map = new HashMap<>();
+
+        while (true)
+        {
+            int id = Integer.parseInt(reader.readLine());
+            String name = reader.readLine();
+            if (name.isEmpty()) {
+                break;
+            } else {
+                map.put(name, id);
+            }
+        }
+        for (Map.Entry<String, Integer> entry : map.entrySet())
+            System.out.println(entry.getValue() + " " + entry.getKey());
+    }
+}
