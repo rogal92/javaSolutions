8f9cfa7bbdbc65143f9609bfc948844cc971cfb5
rogal92 <32602522+rogal92@users.noreply.github.com>
2018-05-20 22:15:16 +0200

    katas

--- src/codewars/AggregationKata.java
+++ src/codewars/AggregationKata.java
@@ -1,5 +1,14 @@
 package codewars;
 
 public class AggregationKata {
-
+    public static Map<String, Long> getNumberOfStudentsByDepartment(Stream<Student> students) {
+    	
+    	Map<String, Long> result = new HashMap<>();
+    	List<String> departments = students.map(s -> s.getDepartment())
+    	.collect(Collectors.toList());
+    	
+    	result = departments.stream().collect(Collectors.groupingBy(s -> s, Collectors.counting()));
+    	
+		return result;
+    }
 }
