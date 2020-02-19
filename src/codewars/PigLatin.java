f84a0e57f2021249c7559d5fa0465c274dfbd289
rogal92 <32602522+rogal92@users.noreply.github.com>
2020-02-19 21:36:25 +0100

    PigLatin

--- src/codewars/PigLatin.java
+++ src/codewars/PigLatin.java
@@ -1,11 +1,27 @@
+
+package main.trie.practise;
+
 import java.util.Arrays;
+import java.util.List;
 
 public class PigLatin {
     public static void main(String[] args) {
-
+        System.out.println(
+                pigIt("Is pig Latin cool ?")
+        );
     }
+
     public static String pigIt(String str) {
-        Arrays.asList(str.split(" ")).stream()
-                .map()
+        List<String> list = Arrays.asList(str.split(" "));
+        String result = "";
+        for (String word: list) {
+            if (word.matches("[a-zA-Z]+")) {
+                String firstChar = String.valueOf(word.charAt(0));
+                result += word.substring(1).concat(firstChar).concat("ay ");
+            } else {
+                result += word;
+            }
+        }
+        return result.trim();
     }
 }
