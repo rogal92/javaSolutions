57169c875e16792632ac43159db28e806366640b
rogal92 <32602522+rogal92@users.noreply.github.com>
2020-02-19 21:35:16 +0100

    PigLatin

---
+++ src/codewars/PigLatin.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class PigLatin {
+    public static void main(String[] args) {
+
+    }
+    public static String pigIt(String str) {
+        Arrays.asList(str.split(" ")).stream()
+                .map()
+    }
+}
